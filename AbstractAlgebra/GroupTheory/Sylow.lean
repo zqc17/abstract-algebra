85f4e14606c2321f9118a21de515e6ac10f97425
zqc17 <zhangqinchuan17@163.com>
2025-04-01 21:27:54 +0800

    Index of Sylow p-group is $|G|/p^k$.

--- AbstractAlgebra/GroupTheory/Sylow.lean
+++ AbstractAlgebra/GroupTheory/Sylow.lean
@@ -6,3 +6,13 @@ variable {G : Type*} [Group G] [Finite G]
 lemma sylow_normal_of_card_eq_one {p : ℕ} [Fact p.Prime] (h : Nat.card (Sylow p G) = 1) :
     (default : Sylow p G).Normal := by
   rwa [← Subgroup.normalizer_eq_top_iff, ← Subgroup.index_eq_one, ← Sylow.card_eq_index_normalizer]
+
+/-- Index of Sylow p-group is $|G|/p^k$. -/
+lemma index_sylow_eq_ord_compl {p : ℕ} [Fact p.Prime] (P : Sylow p G) :
+    P.index = Nat.card G / p ^ (Nat.card G).factorization p := by
+  apply Nat.eq_div_of_mul_eq_left
+  . have : 2 ≤ p := Nat.Prime.two_le Fact.out
+    positivity
+  . rw [mul_comm]
+    convert Subgroup.card_mul_index (P : Subgroup G)
+    exact (Sylow.card_eq_multiplicity P).symm
